dbfbd6c6a8f50082236e141bd279b4540d6a12ba
It's me, Nik! <72689821+MortalDew@users.noreply.github.com>
2024-05-11 10:50:30 +0000

    5

--- api/cron/cron.py
+++ api/cron/cron.py
@@ -2,5 +2,5 @@ import warnings
 from django.http import HttpResponse
 
 def warn(request):
-  warnings.warn("Warning...........Message")
-  return HttpResponse("warn", status:200, reason_phrase:"warn_him")
+    warnings.warn("Warning...........Message")
+    return HttpResponse("warn", status=200, reason_phrase="warn_him")
